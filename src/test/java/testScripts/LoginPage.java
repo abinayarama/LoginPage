f07118589e05d5c71e9d80c16a371c217f098716
abinayarama <abinayaramachandran88@gmail.com>
2023-11-20 13:32:40 +0000

    First version of login page

---
+++ src/test/java/testScripts/LoginPage.java
@@ -0,0 +1,16 @@
+package testScripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://id.heroku.com/login");
+		
+
+	}
+
+}
