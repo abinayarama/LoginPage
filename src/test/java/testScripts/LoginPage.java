cb7e75f09b88664d5ab94a9628746a0113d3d7de
abinayarama <abinayaramachandran88@gmail.com>
2023-11-21 13:28:04 +0000

    2nd updated version

--- src/test/java/testScripts/LoginPage.java
+++ src/test/java/testScripts/LoginPage.java
@@ -1,6 +1,10 @@
 package testScripts;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginPage {
@@ -8,8 +12,21 @@ public class LoginPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriver driver=new ChromeDriver();
-		driver.get("https://id.heroku.com/login");
-		System.out.println("New branch created");
+		driver.get("https://demo.seleniumeasy.com/table-sort-search-demo.html");
+		//System.out.println("New branch created");
+		WebElement salary = driver.findElement(By.xpath("//td[contains(text(),'B. Greer')]//following-sibling::td[5]"));
+		System.out.println(salary.getText());
+		List <WebElement> items= driver.findElements(By.xpath("//td[contains(text(),'B. Greer')]//following-sibling::td"));
+		System.out.println("Size :"+ items.size());
+		System.out.println("Details of Greer");
+		for (WebElement cell:items)
+			
+			System.out.println(cell.getText());
+		List <WebElement> namelist = driver.findElements(By.xpath("//td[contains(text(),'San Francisco')]//preceding-sibling::td[2]"));
+		System.out.println("Name list - San Francisco");
+		for (WebElement cell2:namelist)
+			System.out.println(cell2.getText());
+		driver.close();
 		
 
 	}
